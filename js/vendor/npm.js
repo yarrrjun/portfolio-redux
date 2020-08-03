f1a09083b903f4c167a46f0f88ac8b993c5dd811
Kornajun Roy <yarrrjun@gmail.com>
2020-08-03 04:41:57 -0400

    added images and links to all deployed group projects

---
+++ js/vendor/npm.js
@@ -0,0 +1,24 @@
+div>'+
+							'<div class="datepicker-years">'+
+								'<table class="table-condensed">'+
+									DPGlobal.headTemplate+
+									DPGlobal.contTemplate+
+									DPGlobal.footTemplate+
+								'</table>'+
+							'</div>'+
+						'</div>';
+
+	$.fn.datepicker.DPGlobal = DPGlobal;
+
+
+	/* DATEPICKER NO CONFLICT
+	* =================== */
+
+	$.fn.datepicker.noConflict = function(){
+		$.fn.datepicker = old;
+		return this;
+	};
+
+	/* DATEPICKER VERSION
+	 * =================== */
+	$.fn.datepi
